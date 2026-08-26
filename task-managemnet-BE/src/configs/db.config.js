3d0efdf9574f94943b45b526456c3577f1240d8c
anupamsinha18 <18anupam.sinha@gmail.com>
2026-08-26 16:11:53 +0530

    Support both MONGO_URI and MONGO_URL in db config

--- task-managemnet-BE/src/configs/db.config.js
+++ task-managemnet-BE/src/configs/db.config.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');   
 require('dotenv').config();
 
-const connectToDB=async()=>{
+const connectToDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        const uri = process.env.MONGO_URI || process.env.MONGO_URL;
+        if (!uri) {
+            console.warn("Warning: Neither MONGO_URI nor MONGO_URL is set.");
+            return;
+        }
+        await mongoose.connect(uri);
         console.log("Connected to database successfully");
     } catch (error) {
         console.error("Error connecting to database:", error);
     }
 }
 
-module.exports=connectToDB;
+module.exports = connectToDB;
